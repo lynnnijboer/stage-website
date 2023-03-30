200f6e65e4060e2b5639373b35b33d6bf4466b6c
Lynn Nijboer <lynnnijboer12@gmail.com>
2023-03-30 16:21:57 +0200

    added a nav

---
+++ components/navMenu.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <nav>
+    <ul class="d-flex list-unstyled justify-content-end">
+      <li class="list-item p-3"><a class="text-color-dark" href="#">home</a></li>
+      <li class="list-item p-3"><a class="text-color-dark" href="#">projecten</a></li>
+      <li class="list-item p-3"><a class="text-color-dark" href="#">contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'navMenu'
+}
+</script>
