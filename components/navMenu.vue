4ff0c22537e09dbe5d374e6adb0245d9782bfdcd
Lynn Nijboer <lynnnijboer12@gmail.com>
2023-04-07 11:55:28 +0200

    lol

--- components/navMenu.vue
+++ components/navMenu.vue
@@ -1,7 +1,7 @@
 
 <template>
   <nav>
-    <ul class="d-flex list-unstyled justify-content-end">
+    <ul class="nav-items d-flex list-unstyled justify-content-end">
       <li class="list-item p-3"><a class="text-color-dark" href="#">home</a></li>
       <li class="list-item p-3"><a class="text-color-dark" href="#">projecten</a></li>
       <li class="list-item p-3"><a class="text-color-dark" href="#">contact</a></li>
@@ -15,3 +15,13 @@ export default {
   name: 'navMenu'
 }
 </script>
+
+<style lang="scss" scoped>
+.nav-items {
+  flex-direction: column;
+
+  @include media-breakpoint-up(md) {
+    flex-direction: row;
+  }
+}
+</style>
