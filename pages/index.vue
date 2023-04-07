5240c445f02c6e0d9be95f632a9dfd87a8c781af
Lynn Nijboer <lynnnijboer12@gmail.com>
2023-04-07 16:41:17 +0200

    hohwefo

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="modules">
-    <modules />
+  <div class="container">
+    <div class="modules">
+      <modules />
+    </div>
+    <caseBlockModule/>
   </div>
 </template>
 
 <script>
 import modules from "../components/modules.vue";
-
+import caseBlockModule from "../components/modules/caseBlockModule.vue";
 export default {
   name: 'IndexPage',
-  components: { modules }
+  components: { modules, caseBlockModule }
 }
 </script>
