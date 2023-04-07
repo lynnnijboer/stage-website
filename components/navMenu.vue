7e8bf86fcbb9827d66b8e43a677da493e4218e8b
Lynn Nijboer <lynnnijboer12@gmail.com>
2023-04-07 15:18:26 +0200

    added a button

---
+++ components/navMenu.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <nav>
+    <ul class="nav-items d-flex list-unstyled justify-content-end px-2">
+      <li class="list-item p-3"><a class="text-color-dark" href="#">home</a></li>
+      <li class="list-item p-3"><a class="text-color-dark" href="#">projecten</a></li>
+      <li class="list-item p-3"><a class="text-color-dark" href="#">contact</a></li>
+      <uiButton/>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'navMenu',
+  components: { uiButton }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav-items {
+  flex-direction: column;
+
+  @include media-breakpoint-up(md) {
+    flex-direction: row;
+  }
+}
+</style>
---
+++ components/uiButton.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <button class="ui-button">{{ buttonText }}</button>
+</template>
+
+<script>
+
+export default {
+  name: 'uiButton',
+  props: {
+    buttonText: {
+      type: String,
+      default: "View all projects"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ui-button {
+  background-color: var(--color-alpha);
+  padding: rem(10px);
+  color: var(--color-light);
+}
+</style>
