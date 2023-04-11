49893ab7cf5a3414ed77ae0d143ddead34d18147
Lynn Nijboer <lynnnijboer12@gmail.com>
2023-04-11 10:54:16 +0200

    changed classname of title

--- components/modules/heroModule.vue
+++ components/modules/heroModule.vue
@@ -3,8 +3,8 @@
     <div class="heroModule">
       <div class="heroModule__wrapper">
         <div class="heroModule__content">
-          <h1 class="heroModule__content-title h3">{{ title }}</h1>
-          <p class="heroModule__content-text">{{ text }}</p>
+          <h1 class="heroModule__content--title h3">{{ title }}</h1>
+          <p class="heroModule__content--text">{{ text }}</p>
           <uiButton color="alpha"/>
         </div>
       </div>
@@ -44,7 +44,7 @@ export default {
       grid-row: 1 / 3;
       grid-column: 1;
 
-      &-text {
+      &--text {
         width: 100%;
         max-width: 450px;
         margin-bottom: 40px;
