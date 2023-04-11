44ed37806b16b316b370f5fdc22248a09314f69c
Lynn Nijboer <lynnnijboer12@gmail.com>
2023-04-11 10:33:21 +0200

    made a hero for the website

--- components/navMenu.vue
+++ components/navMenu.vue
@@ -1,11 +1,11 @@
 
 <template>
   <nav>
-    <ul class="nav-items d-flex list-unstyled justify-content-end py-2 px-2">
+    <ul class="nav-items d-flex list-unstyled justify-content-end px-2">
       <li class="list-item p-3"><a class="text-color-dark" href="#">home</a></li>
       <li class="list-item p-3"><a class="text-color-dark" href="#">projecten</a></li>
       <li class="list-item p-3"><a class="text-color-dark" href="#">contact</a></li>
-      <uiButton color="alpha"/>
+      <uiButton class="nav-button" color="alpha"/>
     </ul>
   </nav>
 </template>
@@ -22,6 +22,11 @@ export default {
 <style lang="scss" scoped>
 .nav-items {
   flex-direction: column;
+  align-items: center;
+
+  .nav-button {
+    height: 100%;
+  }
 
   @include media-breakpoint-up(md) {
     flex-direction: row;
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <div class="modules">
       <modules />
     </div>
+    <heroModule/>
     <caseBlockModule/>
   </div>
 </template>
@@ -10,8 +11,9 @@
 <script>
 import modules from "../components/modules.vue";
 import caseBlockModule from "../components/modules/caseBlockModule.vue";
+import heroModule from "../components/modules/heroModule.vue";
 export default {
   name: 'IndexPage',
-  components: { modules, caseBlockModule }
+  components: { modules, caseBlockModule, heroModule }
 }
 </script>
